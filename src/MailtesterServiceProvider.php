93b61255a58924d87581ae130d5dafee20020221
Drakakis.Vbox <lolly@lollypop.gr>
2017-02-21 11:57:33 +0200

    Added swiftMailer as dependecy

--- src/MailtesterServiceProvider.php
+++ src/MailtesterServiceProvider.php
@@ -15,22 +15,11 @@ class MailtesterServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->setupConfig($this->app);
-    }
-
-    /**
-     * Setup the config.
-     *
-     * @param \Illuminate\Contracts\Container\Container $app
-     *
-     * @return void
-     */
-    protected function setupConfig(Container $app)
-    {
         $configPath = __DIR__ . '/../config/mailtester.php';
         $this->publishes([$configPath => config_path('mailtester.php')]);
         $this->mergeConfigFrom($configPath, 'mailtester');
     }
+
 
     public function register()
     {
